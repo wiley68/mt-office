f37c2f69bdb4d47aa0d602b2758902458c110c68
wiley68 <ilko.iv@gmail.com>
2025-06-28 17:52:26 +0300

    initialize project

---
+++ mt-office.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name:       MT Office
+ * Plugin URI:        https://avalonbg.com/maxtrade-office/
+ * Description:       Office application.
+ * Version:           1.0.0
+ * Author:            Avalon Ltd.
+ * Author URI:        https://avalonbg.com
+ * Text Domain:       mt-office
+ * Domain Path:       /languages
+ * License:           GPL-3.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package MT-Office
+ */
+
+// Exit if accessed directly.
+if (! defined('ABSPATH')) {
+	exit;
+}
